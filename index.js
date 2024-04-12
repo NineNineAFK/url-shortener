657af97ab6e9fccc8d32941d3d7bc8646829fd79
Aadity Dawkar <dawkarad2002@gmail.com>
2024-04-13 02:19:18 +0530

    progress

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 const express = require("express");
 const app= express();
+const path = require("path");
 app.use(express.json());
 const router= express.Router();
 const urlRouter= require("./routes/url");
 app.use("/url", urlRouter)
 
+app.set("view engine","ejs");
+app.set("views", path.resolve("./views"));
 
 const{connectMongoDB}= require('./connect')
 connectMongoDB('mongodb://127.0.0.1:27017/newDb')
 
 
+app.get("/test", (req, res)=>
+{
+    return res.render('home')
 
+})
 
 app.listen(3000);
